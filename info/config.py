60c7c52b25834caf6e81c3071b6493c7726b8974
Sanchous2363 <151240556+Sanchous2363@users.noreply.github.com>
2024-05-07 21:09:40 +0300

    Update config.py

--- info/config.py
+++ info/config.py
@@ -1,4 +1,4 @@
-TOKEN = "6941186927:AAGKbBuLkVXZXFfEVEBASSaitww_1jKPADY"
+TOKEN = ""
 MAX_TOKENS = 100 
 MAX_PROJECT_TOKENS = 15000
 MAX_PROJECT_FOR_TTS_TEXT = 15000
@@ -8,6 +8,4 @@ MAX_BLOCKS_ON_ALL = 50
 MAX_LEN_OF_VOICE = 60
 FOR_PROMPT = "Ты бот помошник, твоя задача выполнить команды пользователя, давай краткие, но понятные ответы"
 FOR_PROMPT_IF_DIALOG = "Ты - бот, который поддерживает разговор, задает вопросы пользователю, пытается понять тематику разговора!"
-"""
-folder_id and iam_TOKEN В creds
-"""
+
